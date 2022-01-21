298b0150416ead0b91db5e8186b446cc1d4caea2
Jimbozz <79139294+Jimbozz@users.noreply.github.com>
2022-01-21 12:15:56 +0100

    updated nav and footer

---
+++ script.js
@@ -0,0 +1,12 @@
+const navDrawer = document.querySelector('.nav');
+const button = document.querySelector('.navbar-toggler');
+
+button.onclick = function changeContent() {
+
+  navDrawer.classList.toggle("nav-shadow");
+}
+
+
+
+
+
